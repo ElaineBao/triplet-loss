6f125f614a4de7f394ded8c4577e1d3b75d9410f
ElaineBao <elaine.bao@foxmail.com>
2016-12-14 11:10:55 +0800

    triplet loss

---
+++ tripletloss/config.py
@@ -0,0 +1,15 @@
+import os
+from numpy import *
+
+# Training image data path
+IMAGEPATH = '/tripletloss/data/data-CASIA/'
+
+# Snapshot iteration 
+SNAPSHOT_ITERS = 10000
+
+# Max training iteration
+MAX_ITERS = 400000
+
+# The number of samples in each minibatch
+BATCH_SIZE = 30
+
